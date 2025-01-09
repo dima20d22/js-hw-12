5643a8b2f7c8d64d876e2344ea44f34aad311909
dima20d22 <dimasurovatka22@gmail.com>
2025-01-09 16:24:25 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import glob from 'glob'
 import injectHTML from 'vite-plugin-html-inject'
 import FullReload from 'vite-plugin-full-reload'
 import SortCss from 'postcss-sort-media-queries'
+import ghPages from 'vite-plugin-gh-pages'
 
 export default defineConfig(({ command }) => {
 	return {
